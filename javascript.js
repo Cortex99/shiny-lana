fdf41ea186f68bbaca762a977ee9f490b945b99e
Tommie <tommiecortex99@gmail.com>
2015-06-01 20:04:42 +0200

    Test javascript

--- javascript.js
+++ javascript.js
@@ -1,31 +1,21 @@
-Crafty.init(500,300, document.getElementById('game'));
+var drawScene1 = function() {
+    // Scene 1
+    background(235, 247, 255);
+    fill(0, 85, 255);
+    textSize(39);
+    text("The Story of Winston", 10, height/2);
+};
 
-Crafty.e('Floor, 2D, Canvas, Color')
-  .attr({x: 0, y: 250, w: 200, h: 10})
-  .color('green');
+var drawScene2 = function() {
+    background(173, 239, 255);
+    fill(7, 14, 145);
+    text("Lil Winston is born!", 10, 100);
+    image(getImage("creatures/BabyWinston"), width/2, height/2);
+};
 
-Crafty.e('SecondFloor, 2D, Canvas, Color')
-  .attr({x: 250, y: 230, w: 200, h: 10})
-  .color('red')
+mouseClicked = function() {
+    drawScene2();
+};
 
-Crafty.background('#FFFFFF url(http://samenvattingleren.nl/img/img2.jpg) no-repeat center center');
-
-Crafty.e('2D, Canvas, Color, Fourway, Gravity')
-  .attr({x: 25, y: 5, w: 50, h: 50})
-  .color('orange')
-  .fourway(4)
-  .gravity("Floor")
-  .bind('KeyDown', function(e) {
-    if(e.key == Crafty.keys.LEFT_ARROW) {
-      this.x = this.x - 1;
-    } else if (e.key == Crafty.keys.RIGHT_ARROW) {
-          this.x = this.x + 1;
-    } else if (e.key == Crafty.keys.UP_ARROW) {
-      this.y = this.y - 1;
-    } else if (e.key == Crafty.keys.DOWN_ARROW) {
-      this.y = this.y + 1;
-    } else if (e.key == Crafty.keys.ESC) {
-        this.x = 0;
-    }
-  })
-
+drawScene1();
+//drawScene2();
